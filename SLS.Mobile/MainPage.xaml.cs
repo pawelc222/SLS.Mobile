ad325ec0b83355973a0e86c83887943c6efa2b97
pawelc222 <pawelc222@gmail.com>
2014-12-12 02:57:35 +0100

    Agent niedzialajacy

--- SLS.Mobile/MainPage.xaml.cs
+++ SLS.Mobile/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#define DEBUG_AGENT
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -22,12 +23,21 @@ using SLS.Mobile.ViewModels;
 using SLS.Phone.DbLibrary;
 using Book = SLS.Phone.DbLibrary.Classes.Book;
 
+
 namespace SLS.Mobile
 {
     public partial class MainPage : PhoneApplicationPage
     {
        // private BookViewModel selectedBook;
         // Constructor
+
+        PeriodicTask periodicTask;
+        ResourceIntensiveTask resourceIntensiveTask;
+
+        string periodicTaskName = "PeriodicAgent";
+        string resourceIntensiveTaskName = "ResourceIntensiveAgent";
+        public bool agentsAreEnabled = true;
+
         private bool takingPhoto = false;
         public MainPage()
         {
@@ -241,6 +251,125 @@ namespace SLS.Mobile
             App.ViewModel.LoadDataFromStorage(App.IsolatedData.BorrowedBooks);
             BooksListBox.ItemsSource = null;
             BooksListBox.ItemsSource = App.ViewModel.Items;
+        }
+        private void RemoveAgent(string name)
+        {
+            try
+            {
+                ScheduledActionService.Remove(name);
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+        private void StartPeriodicAgent()
+        {
+            // Variable for tracking enabled status of background agents for this app.
+            agentsAreEnabled = true;
+            
+            // Obtain a reference to the period task, if one exists
+            periodicTask = ScheduledActionService.Find(periodicTaskName) as PeriodicTask;
+
+            // If the task already exists and background agents are enabled for the
+            // application, you must remove the task and then add it again to update 
+            // the schedule
+            if (periodicTask != null)
+            {
+                RemoveAgent(periodicTaskName);
+            }
+            
+            periodicTask = new PeriodicTask(periodicTaskName);
+           
+            // The description is required for periodic agents. This is the string that the user
+            // will see in the background services Settings page on the device.
+            periodicTask.Description = "This demonstrates a periodic task.";
+
+            // Place the call to Add in a try block in case the user has disabled agents.
+            try
+            {
+                ScheduledActionService.Add(periodicTask);
+
+                // If debugging is enabled, use LaunchForTest to launch the agent in one minute.
+                #if(DEBUG_AGENT)
+                    ScheduledActionService.LaunchForTest(periodicTaskName, TimeSpan.FromSeconds(10));
+                #endif
+            }
+            catch (InvalidOperationException exception)
+            {
+                if (exception.Message.Contains("BNS Error: The action is disabled"))
+                {
+                    MessageBox.Show("Background agents for this application have been disabled by the user.");
+                    agentsAreEnabled = false;
+                  //PeriodicCheckBox.IsChecked = false;
+                }
+    
+                if (exception.Message.Contains("BNS Error: The maximum number of ScheduledActions of this type have already been added."))
+                {
+                  // No user action required. The system prompts the user when the hard limit of periodic tasks has been reached.
+      
+                }
+                //PeriodicCheckBox.IsChecked = false;
+            }
+            catch (SchedulerServiceException)
+            {
+                // No user action required.
+                //PeriodicCheckBox.IsChecked = false;
+            }
+        }
+        private void StartResourceIntensiveAgent()
+        {
+            // Variable for tracking enabled status of background agents for this app.
+            agentsAreEnabled = true;
+
+            resourceIntensiveTask = ScheduledActionService.Find(resourceIntensiveTaskName) as ResourceIntensiveTask;
+
+            // If the task already exists and background agents are enabled for the
+            // application, you must remove the task and then add it again to update 
+            // the schedule.
+            if (resourceIntensiveTask != null)
+            {
+                RemoveAgent(resourceIntensiveTaskName);
+            }
+
+            resourceIntensiveTask = new ResourceIntensiveTask(resourceIntensiveTaskName);
+
+            // The description is required for periodic agents. This is the string that the user
+            // will see in the background services Settings page on the device.
+            resourceIntensiveTask.Description = "This demonstrates a resource-intensive task.";
+
+            // Place the call to Add in a try block in case the user has disabled agents.
+            try
+            {
+                ScheduledActionService.Add(resourceIntensiveTask);
+                //ResourceIntensiveStackPanel.DataContext = resourceIntensiveTask;
+
+                // If debugging is enabled, use LaunchForTest to launch the agent in one minute.
+                #if(DEBUG_AGENT)
+                    ScheduledActionService.LaunchForTest(resourceIntensiveTaskName, TimeSpan.FromSeconds(60));
+                #endif
+            }
+            catch (InvalidOperationException exception)
+            {
+                if (exception.Message.Contains("BNS Error: The action is disabled"))
+                {
+                    MessageBox.Show("Background agents for this application have been disabled by the user.");
+                    agentsAreEnabled = false;
+      
+                }
+                //ResourceIntensiveCheckBox.IsChecked = false;
+            }     
+            catch (SchedulerServiceException)
+            {
+                // No user action required.
+                // ResourceIntensiveCheckBox.IsChecked = false;
+            }
+        }
+
+        private void StartPeriodicAgent_OnClick(object sender, EventArgs e)
+        {
+            StartPeriodicAgent();
+            //StartResourceIntensiveAgent();
         }
     }
 }
---
+++ ScheduledTaskAgent1/ScheduledAgent.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows;
+using Microsoft.Phone.Scheduler;
+using Microsoft.Phone.Shell;
+using SLS.Phone.DbLibrary;
+
+namespace ScheduledTaskAgent1
+{
+    public class ScheduledAgent : ScheduledTaskAgent
+    {
+        private static volatile bool _classInitialized;
+
+        /// <remarks>
+        /// ScheduledAgent constructor, initializes the UnhandledException handler
+        /// </remarks>
+        public ScheduledAgent()
+        {
+            if (!_classInitialized)
+            {
+                _classInitialized = true;
+                // Subscribe to the managed exception handler
+                Deployment.Current.Dispatcher.BeginInvoke(delegate
+                {
+                    Application.Current.UnhandledException += ScheduledAgent_UnhandledException;
+                });
+            }
+        }
+
+        /// Code to execute on Unhandled Exceptions
+        private void ScheduledAgent_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // An unhandled exception has occurred; break into the debugger
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        /// <summary>
+        /// Agent that runs a scheduled task
+        /// </summary>
+        /// <param name="task">
+        /// The invoked task
+        /// </param>
+        /// <remarks>
+        /// This method is called when a periodic or resource intensive task is invoked
+        /// </remarks>
+        protected override void OnInvoke(ScheduledTask task)
+        {
+            //TODO: Add code to perform your task in background
+
+            string toastMessage = "";
+
+            // If your application uses both PeriodicTask and ResourceIntensiveTask
+            // you can branch your application code here. Otherwise, you don't need to.
+            if (task is PeriodicTask)
+            {
+                // Execute periodic task actions here.
+                toastMessage = "Periodic task running.";
+            }
+            else
+            {
+                // Execute resource-intensive task actions here.
+                toastMessage = "Resource-intensive task running.";
+            }
+
+            // Launch a toast to show that the agent is running.
+            // The toast will not be shown if the foreground application is running.
+            
+
+            ShellToast toast = new ShellToast();
+            toast.Title = "Agent is working";
+            toast.Content = toastMessage;
+            toast.Show();
+
+#if DEBUG_AGENT
+  ScheduledActionService.LaunchForTest(task.Name, TimeSpan.FromSeconds(10));
+#endif
+            SLSMobileDataProvider dp = new SLSMobileDataProvider();
+            dp.SyncData();
+            NotifyComplete();
+        }
+    }
+}
